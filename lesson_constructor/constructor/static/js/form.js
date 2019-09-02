39d085068f3df74307441fb6c30f2cbaacf15889
Jumas <kbbyfl91@gmail.com>
2019-09-02 05:02:00 +0300

    beautify forms

---
+++ lesson_constructor/constructor/static/js/form.js
@@ -0,0 +1,4 @@
+$(".helptext").addClass("form-text text-muted");
+$("p").addClass("form-group");
+$("input:not([type='submit'])").addClass("form-control");
+$("input[type='submit']").addClass("btn btn-primary");
